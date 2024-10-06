1a04302cc8bb8a7d7d54a9f2fd3b9a5733d6b798
Raphael Zimmermann <mail@raphael.li>
2024-10-06 11:56:14 +0200

    build: Use centralized repository declaration
    See also
    https://docs.gradle.org/current/userguide/declaring_repositories.html#sub:centralized-repository-declaration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,18 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+    }
+}
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
 }
 
 rootProject.name = "zack-bum"
