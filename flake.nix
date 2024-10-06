ce2f10065d2ef24d0008f7da6afcd169b1971153
Raphael Zimmermann <mail@raphael.li>
2024-10-06 12:01:44 +0200

    build: Add gradle to flake
    Uses the same gradle version as declared the gradle wrapper

--- flake.nix
+++ flake.nix
@@ -13,9 +13,33 @@
   in {
     devShells = forAllSystems (pkgs: let
       jdk = pkgs.jdk21_headless;
+
+      # use same gradle as the gradle wrapper
+      gradle = pkgs.callPackage (pkgs.gradleGen (let
+        wrapperProperties = builtins.readFile ./gradle/wrapper/gradle-wrapper.properties;
+        lines = pkgs.lib.strings.splitString "\n" wrapperProperties;
+      in {
+        defaultJava = jdk;
+
+        # version from gradle-wrapper.properties
+        version = let
+          distributionUrlLine = builtins.head (builtins.filter (line: line != null && builtins.match "distributionUrl=.*" line != null) lines);
+          versionMatch = builtins.match ".*/gradle-([^-]*)-bin.zip" distributionUrlLine;
+          versionValue = builtins.elemAt versionMatch 0;
+        in
+          versionValue;
+
+        # hash from gradle-wrapper.properties
+        hash = let
+          sha256SumLine = builtins.head (builtins.filter (line: line != null && builtins.match "distributionSha256Sum=.*" line != null) lines);
+          sha256Hex = builtins.elemAt (builtins.match "distributionSha256Sum=(.*)" sha256SumLine) 0;
+          formattedHash = "sha256:" + sha256Hex;
+        in
+          formattedHash;
+      })) {};
     in {
       default = pkgs.mkShellNoCC {
-        buildInputs = [jdk];
+        buildInputs = [jdk gradle];
       };
     });
   };
