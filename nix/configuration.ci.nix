a498506fe77bf6f7e092af03480b4241ae27db63
Raphael Zimmermann <mail@raphael.li>
2024-10-10 09:34:16 +0200

    build: Add bashInteractive as vscode shell workaround
    See https://github.com/NixOS/nix/issues/6982#issuecomment-1236743200

--- nix/configuration.ci.nix
+++ nix/configuration.ci.nix
@@ -3,6 +3,10 @@
     ./kotlin.nix
   ];
 
+  packages = [
+    pkgs.bashInteractive # see https://github.com/NixOS/nix/issues/6982#issuecomment-1236743200
+  ];
+
   pre-commit.hooks = {
     alejandra.enable = true;
 
