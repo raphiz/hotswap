b62aaf6b954794d0a7ce0aabf6a006c11af8fac5
Raphael Zimmermann <mail@raphael.li>
2024-10-10 09:30:30 +0200

    build: apply gradle reproducibility settings

--- buildSrc/src/main/kotlin/io.github.raphiz.zack-bum.kotlin-library.gradle.kts
+++ buildSrc/src/main/kotlin/io.github.raphiz.zack-bum.kotlin-library.gradle.kts
@@ -2,3 +2,11 @@ plugins {
     id("io.github.raphiz.zack-bum.kotlin")
     `java-library`
 }
+
+// see https://docs.gradle.org/current/userguide/working_with_files.html#sec:reproducible_archives
+tasks.withType<AbstractArchiveTask>().configureEach {
+    isPreserveFileTimestamps = false
+    isReproducibleFileOrder = true
+    dirPermissions { unix("755") }
+    filePermissions { unix("644") }
+}
