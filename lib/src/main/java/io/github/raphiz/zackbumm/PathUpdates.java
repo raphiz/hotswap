aa3995d139e8c1c808f33c69c8f92cdb49858e8f
Raphael Zimmermann <mail@raphael.li>
2024-11-02 10:41:16 +0100

    feat: Add withCreated Method to PathUpdates

--- lib/src/main/java/io/github/raphiz/zackbumm/PathUpdates.java
+++ lib/src/main/java/io/github/raphiz/zackbumm/PathUpdates.java
@@ -1,6 +1,8 @@
 package io.github.raphiz.zackbumm;
 
 import java.nio.file.Path;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 
 public record PathUpdates(
@@ -10,5 +12,25 @@ public record PathUpdates(
 ) {
     public PathUpdates() {
         this(Set.of(), Set.of(), Set.of());
+    }
+
+    public PathUpdates withCreated(Path path) {
+        return new PathUpdates(
+                plusElement(created, path),
+                minusElement(modified, path),
+                minusElement(deleted, path)
+        );
+    }
+
+    private static Set<Path> plusElement(Set<Path> set, Path element) {
+        Set<Path> newSet = new HashSet<>(set);
+        newSet.add(element);
+        return Collections.unmodifiableSet(newSet);
+    }
+
+    private static Set<Path> minusElement(Set<Path> set, Path element) {
+        Set<Path> newSet = new HashSet<>(set);
+        newSet.remove(element);
+        return Collections.unmodifiableSet(newSet);
     }
 }
--- lib/src/test/java/io/github/raphiz/zackbumm/PathUpdatesTest.java
+++ lib/src/test/java/io/github/raphiz/zackbumm/PathUpdatesTest.java
@@ -2,6 +2,10 @@ package io.github.raphiz.zackbumm;
 
 import org.junit.jupiter.api.Test;
 
+import java.nio.file.Path;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PathUpdatesTest {
@@ -13,4 +17,38 @@ class PathUpdatesTest {
         assertTrue(pathUpdates.modified().isEmpty());
         assertTrue(pathUpdates.deleted().isEmpty());
     }
+
+    @Test
+    void withCreatedAddsPathToCreatedSet() {
+        PathUpdates emptyPathUpdates = new PathUpdates();
+        Path aPath = Path.of("/path/to/something");
+
+        PathUpdates actualPathUpdates = emptyPathUpdates.withCreated(aPath);
+
+        PathUpdates expectedPathUpdates = new PathUpdates(Set.of(aPath), Set.of(), Set.of());
+        assertEquals(expectedPathUpdates, actualPathUpdates);
+    }
+
+    @Test
+    void withCreatedRemovesPathFromModifiedIfPresent() {
+        Path aPath = Path.of("/path/to/something");
+        PathUpdates pathUpdatesWithModified = new PathUpdates(Set.of(), Set.of(aPath), Set.of());
+
+        PathUpdates actualPathUpdates = pathUpdatesWithModified.withCreated(aPath);
+
+        PathUpdates expectedPathUpdates = new PathUpdates(Set.of(aPath), Set.of(), Set.of());
+        assertEquals(expectedPathUpdates, actualPathUpdates);
+    }
+
+    @Test
+    void withCreatedRemovesPathFromDeletedIfPresent() {
+        Path aPath = Path.of("/path/to/something");
+        PathUpdates pathUpdatesWithModified = new PathUpdates(Set.of(), Set.of(), Set.of(aPath));
+
+        PathUpdates actualPathUpdates = pathUpdatesWithModified.withCreated(aPath);
+
+        PathUpdates expectedPathUpdates = new PathUpdates(Set.of(aPath), Set.of(), Set.of());
+        assertEquals(expectedPathUpdates, actualPathUpdates);
+    }
+
 }
