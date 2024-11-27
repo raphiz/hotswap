b1c0a5a93f2fba28ee76ce22cbf55f2f4a06d9a8
Raphael Zimmermann <mail@raphael.li>
2024-11-27 07:17:07 +0100

    refactor: Isolate task provider as preparation for a gradle extension

--- gradle-plugin/src/main/java/io/github/raphiz/zackboom/gradle/ZackbummGradlePlugin.java
+++ gradle-plugin/src/main/java/io/github/raphiz/zackboom/gradle/ZackbummGradlePlugin.java
@@ -3,29 +3,35 @@ package io.github.raphiz.zackboom.gradle;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.tasks.JavaExec;
+import org.gradle.api.tasks.TaskProvider;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.net.URL;
+import java.util.stream.Collectors;
 
 public class ZackbummGradlePlugin implements Plugin<Project> {
     @Override
     public void apply(@NotNull Project project) {
         project.afterEvaluate((project1) -> {
-            JavaExec runTask = (JavaExec) project1.getTasks().named("run").get();
+            // TODO: Make name configurable via extension
+            // TODO: Add more config options such as timeouts and additional directories to watch
+            TaskProvider<JavaExec> taskProvider = project1.getTasks().named("run", JavaExec.class);
+            JavaExec task = taskProvider.get();
+
             // Add zackbumm library jar to the runtime classpath
-            runTask.setClasspath(runTask.getClasspath().plus(project.files(zackBummLibraryJar())));
+            task.setClasspath(task.getClasspath().plus(project.files(zackBummLibraryJar())));
 
             // Additional parameters to instruct the zackbumm devmode
-            runTask.jvmArgs(
-                    "-Dzackbumm.mainClass=" + runTask.getMainClass().get(),
+            task.jvmArgs(
+                    "-Dzackbumm.mainClass=" + task.getMainClass().get(),
                     "-Dzackbumm.packagePrefixes=com.example",
                     // TODO: Add all source sets instead
                     "-Dzackbumm.classesOutputs=" + project.getLayout().getBuildDirectory().get().file("classes/java/main/")
             );
 
             // Override the main class
-            runTask.getMainClass().set("io.github.raphiz.zackbumm.DevMode");
+            task.getMainClass().set("io.github.raphiz.zackbumm.DevMode");
         });
     }
 
