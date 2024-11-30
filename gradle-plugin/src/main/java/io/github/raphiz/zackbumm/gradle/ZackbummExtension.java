40ebdfa35562ca2643f55c09c922bafabdfcc2a4
Raphael Zimmermann <mail@raphael.li>
2024-11-30 20:55:08 +0100

    feat: Make task name configurable via extension

--- gradle-plugin/src/main/java/io/github/raphiz/zackbumm/gradle/ZackbummExtension.java
+++ gradle-plugin/src/main/java/io/github/raphiz/zackbumm/gradle/ZackbummExtension.java
@@ -1,4 +1,7 @@
 package io.github.raphiz.zackbumm.gradle;
 
+import org.gradle.api.provider.Property;
+
 public interface ZackbummExtension {
+    Property<String> getTaskName();
 }
--- gradle-plugin/src/main/java/io/github/raphiz/zackbumm/gradle/ZackbummGradlePlugin.java
+++ gradle-plugin/src/main/java/io/github/raphiz/zackbumm/gradle/ZackbummGradlePlugin.java
@@ -3,7 +3,6 @@ package io.github.raphiz.zackbumm.gradle;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.tasks.JavaExec;
-import org.gradle.api.tasks.TaskProvider;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
@@ -16,20 +15,20 @@ import java.util.stream.Collectors;
 public class ZackbummGradlePlugin implements Plugin<Project> {
     @Override
     public void apply(@NotNull Project target) {
-        target.getExtensions().create("zackbumm", ZackbummExtension.class);
+        ZackbummExtension extension = target.getExtensions().create("zackbumm", ZackbummExtension.class);
+        extension.getTaskName().convention("run");
+
         target.afterEvaluate((project) -> {
             // TODO: Make name configurable via extension
             // TODO: Add more config options such as timeouts and additional directories to watch
-            TaskProvider<JavaExec> taskProvider = project.getTasks().named("run", JavaExec.class);
+            JavaExec task = target.getTasks().named(extension.getTaskName().get(), JavaExec.class).get();
             String packagePrefix = "com.example";
             Duration debounceDuration = null;
             Duration shutdownPollingInterval = null;
-            String classesOutputs = taskProvider.get().getClasspath().getFiles().stream()
+            String classesOutputs = task.getClasspath().getFiles().stream()
                     .map(File::getAbsolutePath)
                     .filter((file) -> !file.endsWith(".jar"))
                     .collect(Collectors.joining(File.pathSeparator));
-
-            JavaExec task = taskProvider.get();
 
             // Add zackbumm library jar to the runtime classpath
             task.setClasspath(task.getClasspath().plus(project.files(zackBummLibraryJar())));
