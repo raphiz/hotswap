b1a885d7dac738dbbc368f69938d5b4f6a5b87ae
Raphael Zimmermann <mail@raphael.li>
2024-11-30 21:16:41 +0100

    style: Reformat code

--- gradle-plugin/src/main/java/io/github/raphiz/zackbumm/gradle/ZackbummExtension.java
+++ gradle-plugin/src/main/java/io/github/raphiz/zackbumm/gradle/ZackbummExtension.java
@@ -7,7 +7,10 @@ import java.time.Duration;
 
 public interface ZackbummExtension {
     Property<String> getTaskName();
+
     Property<FileCollection> getClassDirectories();
+
     Property<Duration> getDebounceDuration();
+
     Property<Duration> getShutdownPollingInterval();
 }
