532fb75b0edcf1523e488d3049ccce1f080a7f38
Raphael Zimmermann <mail@raphael.li>
2025-04-20 19:46:11 +0200

    build: Prepare gradle plugin release

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -1,13 +1,24 @@
 plugins {
     id("io.github.raphiz.hotswap.java-library")
-    `java-gradle-plugin`
+    id("com.gradle.plugin-publish") version "1.3.1"
+    id("com.gradleup.shadow") version "8.3.6"
+}
+
+tasks.shadowJar.configure {
+    archiveClassifier = ""
 }
 
 gradlePlugin {
+    website.set("https://github.com/raphiz/hotswap")
+    vcsUrl.set("https://github.com/raphiz/hotswap")
     plugins {
         create("hotswap") {
             id = "io.github.raphiz.hotswap"
+            displayName = "Hotswap Gradle Plugin"
+            description =
+                "Java library & Gradle plugin providing lightweight and efficient hot reloading (similar to Spring Boot Devtools) for ANY JVM application"
             implementationClass = "io.github.raphiz.hotswap.gradle.HotswapGradlePlugin"
+            tags = listOf("hotswap", "reload", "refresh")
         }
     }
 }
