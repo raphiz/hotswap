c7b02cccfeaa6b98c88ef8271d37e4618948b3b6
Raphael Zimmermann <mail@raphael.li>
2025-04-28 13:20:55 +0200

    build: Simplify release version by just using  github.ref_name

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,10 +2,6 @@ plugins {
     `kotlin-dsl`
 }
 
-dependencies {
-    implementation("com.palantir.gradle.gitversion:gradle-git-version:3.1.0")
-}
-
 tasks.test {
     useJUnitPlatform()
 }
--- buildSrc/src/main/kotlin/io.github.raphiz.hotswap.project.gradle.kts
+++ buildSrc/src/main/kotlin/io.github.raphiz.hotswap.project.gradle.kts
@@ -1,16 +1,4 @@
-plugins {
-    id("com.palantir.git-version")
-}
-
 group = "io.github.raphiz"
 
-version =
-    run {
-        val gitVersion: groovy.lang.Closure<String> by extra
-        val version = gitVersion()
-        if (version.startsWith("v")) {
-            version.substring(1)
-        } else {
-            version
-        }
-    }
+val rawVersion = System.getenv("PROJECT_VERSION") ?: "SNAPSHOT"
+version = rawVersion.removePrefix("v")
